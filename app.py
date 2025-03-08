6a2f528968f344535f7f8cec81f57e11184641dd
Ruturaj1007 <sanapruturaj2@gmail.com>
2025-03-08 11:22:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from Bundesliga.bundesliga import bundesliga
+from EPL.epl import epl
+from LaLiga.laliga import laliga
+from SerieA.seriea import seriea
+from home import home_page
+from news.football_news_module import display_football_news
+from Player_valuation.player import player
+
+
+def main():
+    st.set_page_config(
+        page_title='Football Match Outcome Predictor',
+        page_icon='⚽',
+        layout='centered',
+        initial_sidebar_state='auto'
+    )
+
+    st.markdown(
+        """
+        <style>
+        
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.sidebar.header("Select the League")
+    selected_page = st.sidebar.radio(
+        "Choose", 
+        ["Home", "Bundesliga", "EPL", "La Liga", "Serie A", "Football News", "Player valuation"]
+    )
+
+    if selected_page == "Home":
+        home_page()
+    elif selected_page == "Bundesliga":
+        bundesliga()
+    elif selected_page == "EPL":
+        epl()
+    elif selected_page == "La Liga":
+        laliga()
+    elif selected_page == "Serie A":
+        seriea()
+    elif selected_page == "Football News":
+        display_football_news()
+    elif selected_page == "Player valuation":
+        player()
+ # Call the football news function
+
+if __name__ == "__main__":
+    main()
---
+++ home.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import Image
+
+def home_page():
+    # Custom CSS for background image
+    st.markdown(
+        """
+        <style>
+        
+        
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    container = st.container()
+
+    with container:
+        st.image("logo1.png", width=150)
+
+        st.title("⚽Football⚽")
+
+        st.write("* This web app predicts the outcome of football matches in the top European leagues like: Bundesliga, EPL, La Liga, Serie A")
+
+
+
+        st.write("* Select the league from the sidebar to get started")
+
+    
+        st.write("* Bundesliga: German Football League")
+        st.text("")
+        st.write("* EPL: English Premier League")
+        st.text("")
+        st.write("* La Liga: Spanish Football League")
+        st.text("")
+        st.write("* Serie A: Italian Football League")
+        st.text("")
+        st.write("* Football News")
+        st.text("")
+        st.write("* Player Valuation")
+        st.text("")
+
+if __name__ == "__main__":
+    home_page()
